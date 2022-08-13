ef652728cd1ceb64c3e2e6671f7c6d3729a38220
joaomandaca <joao.msoares@ufpe.br>
2022-08-13 15:32:39 -0300

    ADC COMENTARIOS

--- Q6.py
+++ Q6.py
@@ -1,23 +1,30 @@
 # questão 6
-
+#LISTA RECEBISDAS PELO USUARIO
 list1 = []
 list2 = []
+
+#NUMERO DE ELEMENTOS DENTRO DA LISTA
 n = 15
 
+#RECEBENDO VALORE PARA AS DUAS LISTA UMA DE CADA VEZ
 for i in range(0, n):
     x = list1.append(input('insira uma informação\nR:'))
 
 for j in range(0, n):
     x = list2.append(input('insira uma informação para a outra lista\nR:'))
 
+#EXIBINDO PARA O USUARIO
 print('lista 1', list1)
 print('lista 2', list2)
-list3 = []
 
+#LISTA INTERPOLADA E VARIAVEIS AUXILIARES
+list3 = []
 rep = 0
 rep0 = 0
 rep1 = 0
 
+
+#INTERPOLANDO LISTA
 while rep != (n*2):
     x = float(rep % 2)
     if x == 0:
@@ -30,6 +37,7 @@ while rep != (n*2):
         rep1 += 1
     rep += 1
 
+#PRINTANDO LISTA COM A INTERPOLAÇÃO DAS DUAS LISTAS JA EXISTENTES
 print('lista 3', list3)
 
 
