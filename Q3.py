302453f8304a3bad6fcaaf2aabef9737b74d4385
joaomandaca <joao.msoares@ufpe.br>
2022-08-13 15:36:30 -0300

    AGORA TA UMA COISA MAIS DECENTE

--- Q3.py
+++ Q3.py
@@ -1,9 +1,11 @@
 #questão 3
 
-print('responda as questões com::\n 0 - confirmação\n 1 - negação\n')
+print('responda as questões com::\n 0 - SIM\n 1 - NÃO\n')
 
+#lista com as pergunatas
 perguntas = ["Já trabalhou com a vítima?","Devia para a vítima?","Mora perto da vítima?","Esteve no local do crime?","Telefonou para a vítima?"]
 
+#fezendo as perguntas ao usuario e contanto o numero de respostas
 r_final = 0
 for x in perguntas:
     print(x)
@@ -13,6 +15,7 @@ for x in perguntas:
     r1 = 0
 print('\n\n')
 
+#analisando repostas para para definir conde o entrevistado se enquadra
 if r_final == 2:
     print('pessoa suspeita')
 elif (r_final >= 3) and (r_final <= 4):
